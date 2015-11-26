d9e96b29d1fb59667f0406489745592b5d8c8873
magicghostvu <magicghost.vu@gmail.com>
2015-11-26 18:28:27 +0700

    fix lối session already invalided

--- webStruts/src/funtionSupport/Session.java
+++ webStruts/src/funtionSupport/Session.java
@@ -5,14 +5,28 @@ import java.util.Map;
 import com.opensymphony.xwork2.ActionContext;
 
 public class Session {
-	private static Map<String, Object> session = ActionContext.getContext().getSession();
+	private static Map<String, Object> session ;
+	static{
+		session = ActionContext.getContext().getSession();
+		System.out.println(session==null);
+	}
+	
 	
 	public static String getSessionUsername() {
-		return (String)session.get("username");
+		String res=null;
+		try {
+			res=(String)session.get("username");
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e);
+		}
+		//return (String)session.get("username");
+		return res;
 	}
 	
 	public static int getSessionId(){
 		return (int)session.get("id");
+		
 	}
 	public static Map<String, Object> getMap(){
 		return session;
